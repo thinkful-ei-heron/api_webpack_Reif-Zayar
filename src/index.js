ea4f2706d8989e5ebe111cc8c08769cc3be3a07f
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-09-30 10:03:26 -0500

    #2 done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api.js';
+
 import shoppingList from './shopping-list';
 
 const main = function () {
@@ -11,6 +13,11 @@ const main = function () {
   // fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
   //   .then(res => res.json())
   //   .then(data => console.log(data));
+
+  // api.getItems()
+  //   .then(res => console.log(res));
+
+  // console.log(api.BASE_URL);
 
   shoppingList.bindEventListeners();
   shoppingList.render();
