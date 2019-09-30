22475cb3e8b3df6fded19ecb192364f5e70c4f43
zkhin <arandomname@gmail.com>
2019-09-30 17:22:46 -0400

    zayar

--- src/api.js
+++ src/api.js
@@ -31,18 +31,24 @@ const deleteItem = function (id) {
     method: 'DELETE',
     headers: { 'Content-Type': 'application/json' }
   });
-}
+};
 
 const responseCheck = function (res) {
-  let error;
   if (!res.ok) {
+<<<<<<< HEAD
     error = `Status code ${res.status} not ok`;
     store.errors.error = error;
     $('.js-shopping-list').append(store.errors.error)
     throw Error(error);
+=======
+    store.errors.error = res.status;
+    document.getElementById('error-container').innerHTML = (`Error: ${res.status}`);
+    return Promise.reject(`Error code: ${res.status}`);
+>>>>>>> f8fef18a09bd0226aebead8b13c8cda8d15057be
   }
-  return res
-}
+  document.getElementById('error-container').innerHTML = '';
+  return res;
+};
 
 const apiFetch = function (...args) {
   return fetch(...args)
@@ -50,10 +56,20 @@ const apiFetch = function (...args) {
       responseCheck(resp);
       return resp;
     })
+<<<<<<< HEAD
     .catch(error => {
       console.log(error);
+=======
+    .catch(resp => {
+      document.getElementById('error-container').innerHTML = '';
+      store.errors.error = resp;
+      document.getElementById('error-container').innerHTML = (`Error: ${resp}`);
+>>>>>>> f8fef18a09bd0226aebead8b13c8cda8d15057be
     });
-}
+
+
+  // .then(resp => responseCheck(resp));
+};
 
 export default {
   getItems,
